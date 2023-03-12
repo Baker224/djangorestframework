66b72fcaceb63c55269184cf1327869e9e875365
Timur <matt_baker@mail.ru>
2023-03-13 00:30:43 +0500

    lesson9

--- authapp/views.py
+++ authapp/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import MyUser
-from .serializers import MyUserModelSerializer
+from .serializers import MyUserModelSerializer, MyUserModelSerializerV2
 
 class MyUserModelViewSet(mixins.RetrieveModelMixin, mixins.CreateModelMixin,
                    mixins.UpdateModelMixin,
                    mixins.ListModelMixin,
                    GenericViewSet):
-    serializer_class = MyUserModelSerializer
+    # serializer_class = MyUserModelSerializer
     queryset = MyUser.objects.all()
+
+    def get_serializer_class(self):
+       if self.request.version == '2.0':
+           return MyUserModelSerializerV2
+       elif self.request.version == '1.0':
+           return MyUserModelSerializer
+       return MyUserModelSerializer
 
