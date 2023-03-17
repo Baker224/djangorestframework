cd4b662a37cd9a861a6b9ea034a143148fc6492d
Timur <matt_baker@mail.ru>
2023-03-18 02:03:07 +0500

    lesson11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import TODOList from './components/TODOList.js'
 import ProjectsList from './components/ProjectsList.js'
 import UserProjectList from './components/UserProjectList.js'
 import LoginForm from './components/LoginForm.js'
+import TODOForm from './components/TODOForm.js'
+import ProjectForm from './components/ProjectForm.js'
+
 
 import {HashRouter, BrowserRouter, Route, Routes, Link, Navigate} from 'react-router-dom'
 
@@ -34,6 +37,48 @@ class App extends React.Component {
                         'token': token}, this.getData)
             })
             .catch(error => console.log(error))
+    }
+
+    create_TODO(text, projects, users) {
+
+    console.log(projects,users.id)
+        let headers = this.getHeaders()
+
+             axios
+            .post('http://127.0.0.1:8000/api/create_TODO/', {'text': text, 'project': projects, 'creater': users}, {headers})
+            .then(response => {
+                this.getData()
+             })
+            .catch(error => {
+                console.log(error)
+                console.log('otladka')
+
+
+            })
+    }
+
+
+
+
+
+
+    create_project(title, repo, users) {
+
+    console.log(title,users.id)
+
+        let headers = this.getHeaders()
+
+             axios
+            .post('http://127.0.0.1:8000/api/project', {'text': title, 'text': repo, 'users': users}, {headers})
+            .then(response => {
+                this.getData()
+             })
+            .catch(error => {
+                console.log(error)
+                console.log('otladka')
+
+
+            })
     }
 
     isAuth() {
@@ -96,6 +141,8 @@ class App extends React.Component {
             .catch(error => console.log(error))
     }
 
+
+
     logOut() {
         let token = localStorage.setItem('token', '')
         this.setState({
@@ -110,6 +157,8 @@ class App extends React.Component {
                 <nav>
                     <li> <Link to='/users'> users </Link> </li>
                     <li> <Link to='/TODO'> TODO </Link></li>
+                    <li> <Link to='/create_TODO'> Create TODO </Link></li>
+                    <li> <Link to='/create_project'> Create Project </Link></li>
                     <li> <Link to='/projects'> projects </Link> </li>
                     <li> {this.isAuth() ? <button onClick={() => this.logOut()} >Logout</button> : <Link to='/login'> Login </Link> } </li>
 
@@ -118,10 +167,14 @@ class App extends React.Component {
                     <Routes>
                         <Route path='/users' >
                             <Route index  element ={<UsersList users = {this.state.users} />} />
-                            <Route path=':userId'  element ={<UserProjectList projects = {this.state.projects} />} />
+                            <Route path=":userId" element={<UserProjectList projects={this.state.projects} />} />
+
                             </Route>
 
                         <Route exact path='/login' element ={<LoginForm obtainAuthToken = {(login, password) => this.obtainAuthToken(login, password)} />} />
+
+                        <Route exact path='/create_TODO' element ={<TODOForm  projects = {this.state.projects}  users = {this.state.users} create_TODO={(text, projects, users) => this.create_TODO(text, projects, users)}  />} />
+                        <Route exact path='/create_project' element ={<ProjectForm  users = {this.state.users} create_project={(title, repo, users) => this.create_project(title, repo, users)}  />} />
 
                         <Route exact path='/TODO' element ={<TODOList TODO = {this.state.TODO} />} />
                         <Route exact path='/projects' element ={<ProjectsList projects = {this.state.projects} />} />
